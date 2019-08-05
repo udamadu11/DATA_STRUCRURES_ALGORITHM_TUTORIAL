4ea2b1fd4197287a5d609b9c29c8edb2b1281a2e
udamadu11 <madu12dara@gmail.com>
2019-08-05 19:14:15 +0530

    insertArrayElement

---
+++ InsertEleGivenLocation.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(){
+    int n,i,loc,key;
+    int arr[20];
+    
+    printf("Enter Size Of Array :");
+    scanf("%d",&n);
+
+    printf("Enter Elements :\n");
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("Enter location To Add :");
+    scanf("%d",&loc);
+
+    printf("Enter Element to Add :");
+    scanf("%d",&key);
+
+    for(i=n-1;i<=loc;i--){
+        arr[i+1] = arr[i];
+    }
+    arr[loc]=key;
+    for(i=0;i<n;i++){
+        printf("%d\n",arr[i]);
+    }
+}
