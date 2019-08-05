caf10192d7df638c9a4af9eeab9fd245885257e3
udamadu11 <madu12dara@gmail.com>
2019-08-05 19:15:28 +0530

    InsertArrayElement

--- InsertEleGivenLocation.c
+++ InsertEleGivenLocation.c
@@ -6,7 +6,7 @@ int main(){
     printf("Enter Size Of Array :");
     scanf("%d",&n);
 
-    printf("Enter Elements :\n");
+    printf("Enter Element :\n");
     for(i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
